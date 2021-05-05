f98e5f4c97dd6b41e98926d25455516fbcf9a5d3
Ingmar <i.kellner@hello2morrow.com>
2021-05-05 12:29:10 +0200

    Update license header.

--- src/main/java/com/hello2morrow/sonargraph/integration/sonarqube/AbstractDataProvider.java
+++ src/main/java/com/hello2morrow/sonargraph/integration/sonarqube/AbstractDataProvider.java
@@ -1,6 +1,6 @@
 /**
  * SonarQube Sonargraph Integration Plugin
- * Copyright (C) 2016-2020 hello2morrow GmbH
+ * Copyright (C) 2016-2021 hello2morrow GmbH
  * mailto: support AT hello2morrow DOT com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
