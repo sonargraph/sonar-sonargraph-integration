d5ea414250702c43f1f0c735e61d09747dc5d50d
Ingmar <i.kellner@hello2morrow.com>
2021-05-11 21:45:30 +0200

    Update test project

--- src/test/smallTestProject/executeSonarMvn.bat
+++ src/test/smallTestProject/executeSonarMvn.bat
@@ -1,5 +1,7 @@
 set JAVA_HOME=C:\Programs\Jdk\openjdk-11.0.1+13
 set PATH=%JAVA_HOME%\bin;%PATH%
-mvn clean package -Dmaven.test.skip=true com.hello2morrow:sonargraph-maven-plugin:11.1.0:create-report -Dsonar.login=admin -Dsonar.password=admin1 -Dsonargraph.systemDirectory=./AlarmClock.sonargraph -Dsonargraph.prepareForSonarQube=true -Dsonargraph.autoUpdate=false -Dsonargraph.installationDirectory=D:\00_repo\sgng-master\products\SonargraphBuild\dist\release\SonargraphBuild -Dsonargraph.licenseFile=C:\Users\Ingmar\AppData\Roaming\hello2morrow\Sonargraph\Standalone\Sonargraph.license sonar:sonar -e
+mvn clean package -Dmaven.test.skip=true com.hello2morrow:sonargraph-maven-plugin:11.2.0:create-report -Dsonar.login=admin -Dsonar.password=admin1 -Dsonargraph.systemDirectory=./AlarmClock.sonargraph -Dsonargraph.prepareForSonarQube=true -Dsonargraph.autoUpdate=false -Dsonargraph.installationDirectory=D:\00_repo\sgng-master\products\SonargraphBuild\dist\release\SonargraphBuild -Dsonargraph.licenseFile=C:\Users\Ingmar\AppData\Roaming\hello2morrow\Sonargraph\Standalone\Sonargraph.license sonar:sonar -e 
 
 rem mvnDebug sonar:sonar -Dsonar.sonargraph.integration:report.path=./AlarmClock.xml
+
+mvnDebug sonar:sonar -Dsonar.login=admin -Dsonar.password=admin1 
